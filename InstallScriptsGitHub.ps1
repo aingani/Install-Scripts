304cfc2a3f0bb047986ad29921a5d7a82dd6f04d
Alfred Ingani <aingani@quantech.net>
2026-07-14 09:01:09 -0400

    Restore and update InstallScriptsGitHub.ps1

--- InstallScriptsGitHub.ps1
+++ InstallScriptsGitHub.ps1
@@ -49,6 +49,7 @@ $scripts = @(
         Name = "Install Adobe Acrobat (Unified)"
         Url  = "https://raw.githubusercontent.com/aingani/install-scripts/main/InstallAcrobatAdobeNew.ps1"
         Note = "New"
+    },
     @{
         Name = "Install WinGet Apps"
         Url  = "https://raw.githubusercontent.com/aingani/install-scripts/main/InstallWingetApps.ps1"
